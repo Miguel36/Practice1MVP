2221ec6341e99065832388665c5fc3c6f814da30
Monti <miguelmontilla.1936@gmail.com>
2019-09-29 00:23:36 -0500

    Implementando la interface en la View y creando una instancia de UserPresenterImpl

--- app/src/main/java/com/example/practice1mvp/view/UserActivity.java
+++ app/src/main/java/com/example/practice1mvp/view/UserActivity.java
@@ -3,12 +3,35 @@ package com.example.practice1mvp.view;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.practice1mvp.R;
+import com.example.practice1mvp.interfaces.UserContract;
+import com.example.practice1mvp.presenter.UserPresenterImpl;
 
-public class UserActivity extends AppCompatActivity {
+import java.util.List;
+
+public class UserActivity extends AppCompatActivity implements UserContract.View {
+
+    private UserPresenterImpl userPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userPresenter = new UserPresenterImpl(this);
+        userPresenter.start();
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public void loadDataInList(List<User> users) {
+
     }
 }
