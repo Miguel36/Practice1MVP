9c6ec61cf99bf4a92251d188be853be414739f46
Miguel36 <miguelmontilla.1936@gmail.com>
2019-10-01 16:26:34 -0500

    Añadiendo funcionalidad al Adapter y ViewHolder del Recycler

--- app/src/main/java/com/example/practice1mvp/usuario/view/RecyclerAdapter.java
+++ app/src/main/java/com/example/practice1mvp/usuario/view/RecyclerAdapter.java
@@ -1,25 +1,37 @@
 package com.example.practice1mvp.usuario.view;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.practice1mvp.R;
+import com.example.practice1mvp.usuario.model.User;
+import java.util.List;
+
 public class RecyclerAdapter extends RecyclerView.Adapter {
 
+    List<User> usersList;
+
+    public RecyclerAdapter(List<User> userslist){
+        this.usersList = userslist;
+    }
 
     @NonNull
     @Override
-    public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item, parent, false);
+        return new RecyclerViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-
+        ((RecyclerViewHolder) holder).binData(usersList.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return usersList.size();
     }
 }
--- app/src/main/java/com/example/practice1mvp/usuario/view/RecyclerViewHolder.java
+++ app/src/main/java/com/example/practice1mvp/usuario/view/RecyclerViewHolder.java
@@ -1,11 +1,28 @@
 package com.example.practice1mvp.usuario.view;
 
 import android.view.View;
+import android.widget.TextView;
+
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.practice1mvp.R;
+import com.example.practice1mvp.usuario.model.User;
+
+import java.util.List;
+
 public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+    TextView idTextView, titleTextView, bodyTextView;
 
     public RecyclerViewHolder(View itemView) {
         super(itemView);
+        idTextView = itemView.findViewById(R.id.user_id);
+        titleTextView = itemView.findViewById(R.id.user_title);
+        bodyTextView = itemView.findViewById(R.id.user_body);
+    }
+
+    public void binData(User user) {
+        idTextView.setText(user.getId());
+        titleTextView.setText(user.getTitle());
+        bodyTextView.setText(user.getBody());
     }
 }
