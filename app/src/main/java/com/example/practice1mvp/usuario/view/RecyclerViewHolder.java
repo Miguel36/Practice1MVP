cb92437df11898d5693c21ca49830f56eafe1f04
Miguel36 <miguelmontilla.1936@gmail.com>
2019-10-01 11:01:27 -0500

    Creando el Adapter y el ViewHolder del RecyclerView

---
+++ app/src/main/java/com/example/practice1mvp/usuario/view/RecyclerViewHolder.java
@@ -0,0 +1,11 @@
+package com.example.practice1mvp.usuario.view;
+
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    public RecyclerViewHolder(View itemView) {
+        super(itemView);
+    }
+}
