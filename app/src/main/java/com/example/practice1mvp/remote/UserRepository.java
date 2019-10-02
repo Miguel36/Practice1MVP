885a1017a7c6c50942b0a9a63bedabc13cf62c75
Miguel36 <miguelmontilla.1936@gmail.com>
2019-10-02 17:52:56 -0500

    Se crea la conexion al servicio para obtener los usuarios

--- app/src/main/java/com/example/practice1mvp/remote/UserRepository.java
+++ app/src/main/java/com/example/practice1mvp/remote/UserRepository.java
@@ -1,11 +1,17 @@
 package com.example.practice1mvp.remote;
 
+import com.example.practice1mvp.usuario.model.User;
+
+import java.util.List;
+
+import retrofit2.Callback;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserRepository {
 
     private static UserRepository userRepository;
+    private WebServices service;
     private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
 
     public static UserRepository getInstance(){
@@ -20,5 +26,11 @@ public class UserRepository {
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
+
+        service = retrofit.create(WebServices.class);
+    }
+
+    public void getUsersInfo(Callback<List<User>> callback){
+        service.getUsers().enqueue(callback);
     }
 }
