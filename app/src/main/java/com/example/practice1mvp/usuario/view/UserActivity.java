856f4e0f6d9d133fc9fa5bad54e5c74c5dcd48d6
Miguel36 <miguelmontilla.1936@gmail.com>
2019-10-03 17:19:16 -0500

    Dando a la vista lo que necesita

--- app/src/main/java/com/example/practice1mvp/usuario/view/UserActivity.java
+++ app/src/main/java/com/example/practice1mvp/usuario/view/UserActivity.java
@@ -1,7 +1,10 @@
 package com.example.practice1mvp.usuario.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
+import android.widget.Toast;
 import com.example.practice1mvp.R;
 import com.example.practice1mvp.usuario.interfaces.UserContract;
 import com.example.practice1mvp.usuario.model.User;
@@ -12,6 +15,8 @@ import java.util.List;
 public class UserActivity extends AppCompatActivity implements UserContract.View {
 
     private UserPresenterImpl userPresenter;
+    private RecyclerView recyclerView;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +28,19 @@ public class UserActivity extends AppCompatActivity implements UserContract.View
 
     @Override
     public void init() {
-
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        userPresenter.loadUsers();
     }
 
     @Override
     public void showError(String message) {
-
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
     @Override
     public void loadDataInList(List<User> users) {
-
+       adapter = new RecyclerAdapter(users);
+       recyclerView.setAdapter(adapter);
     }
 }
